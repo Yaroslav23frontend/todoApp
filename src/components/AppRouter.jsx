e0009b6019ad3ec05ecb82c2cde5abf351efd2c2
Yaroslav Khomenko <kyaroslav180@gmail.com>
2022-06-24 19:19:10 +0300

    LCP optimization

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -16,31 +16,37 @@ import ToDoListPage from "../pages/ToDoListPage";
 import UserDeleted from "../pages/UserDeleted";
 
 export default function AppRouter() {
-  const { isAuth } = useAuth();
+  const { isAuth, loading } = useAuth();
   console.log(isAuth);
   return (
     <Routes>
-      {isAuth ? (
+      {loading === false ? (
         <>
-          <Route path="/boards" element={<Boards />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/resetEmail" element={<ResetEmail />} />
-          <Route path="/resetPassword" element={<ResetPassword />} />
-          <Route exact path="/boards/:id" element={<ToDoListPage />} />
-          <Route path="" element={<Navigate to="/boards" />} />
+          {isAuth ? (
+            <>
+              <Route path="/boards" element={<Boards />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/resetEmail" element={<ResetEmail />} />
+              <Route path="/resetPassword" element={<ResetPassword />} />
+              <Route exact path="/boards/:id" element={<ToDoListPage />} />
+              <Route path="" element={<Navigate to="/boards" />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/reset" element={<Reset />} />
+              <Route path="/resetMassege" element={<ResetRequest />} />
+              <Route path="/emailChanged" element={<EmailChangedSuccess />} />
+              <Route path="/passwordChanged" element={<PasswordChanged />} />
+              <Route path="/userDeleted" element={<UserDeleted />} />
+            </>
+          )}
+          <Route path="*" element={<NotFound />} />
         </>
       ) : (
-        <>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/reset" element={<Reset />} />
-          <Route path="/resetMassege" element={<ResetRequest />} />
-          <Route path="/emailChanged" element={<EmailChangedSuccess />} />
-          <Route path="/passwordChanged" element={<PasswordChanged />} />
-          <Route path="/userDeleted" element={<UserDeleted />} />
-        </>
+        <></>
       )}
-      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
